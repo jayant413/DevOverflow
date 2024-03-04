b1fc24d944aa9132aceddca7d538f330abb88509
jayant413 <jayantsawarkar4103@gmail.com>
2024-03-04 10:13:43 +0530

    feat: home questions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,4 @@
-// import QuestionCard from "@/components/cards/QuestionCard";
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
@@ -12,6 +12,7 @@ import Link from "next/link";
 
 import type { Metadata } from "next";
 import { auth } from "@clerk/nextjs";
+import { title } from "process";
 
 export const metadata: Metadata = {
   title: "Home | Dev Overflow",
@@ -42,6 +43,81 @@ export default async function Home({ searchParams }: SearchParamsProps) {
   //     page: searchParams.page ? +searchParams.page : 1,
   //   });
   // }
+
+  const questions = [
+    {
+      _id: "1",
+      title: "How to implement TypeScript interfaces?",
+      tags: [
+        { _id: "tag1", name: "TypeScript" },
+        { _id: "tag2", name: "Interfaces" },
+      ],
+      author: {
+        _id: "author1",
+        name: "John Doe",
+        picture: "profile1.jpg",
+        clerkId: "clerk1",
+      },
+      upvotes: ["user1", "user2", "user3"],
+      views: 1050,
+      answers: [
+        {
+          text: "You can define interfaces using the 'interface' keyword.",
+          author: "Jane Smith",
+        },
+        {
+          text: "Interfaces help in defining the structure of objects.",
+          author: "Bob Johnson",
+        },
+      ],
+      createdAt: new Date(2024, 1, 15),
+    },
+    {
+      _id: "2",
+      title: "What are the benefits of using React hooks?",
+      tags: [
+        { _id: "tag3", name: "React" },
+        { _id: "tag4", name: "Hooks" },
+      ],
+      author: {
+        _id: "author2",
+        name: "Alice Brown",
+        picture: "profile2.jpg",
+        clerkId: "clerk2",
+      },
+      upvotes: ["user4", "user5"],
+      views: 7500,
+      answers: [],
+      createdAt: new Date(2023, 12, 15),
+    },
+    {
+      _id: "3",
+      title: "How to optimize performance in Node.js applications?",
+      tags: [
+        { _id: "tag5", name: "Node.js" },
+        { _id: "tag6", name: "Performance" },
+      ],
+      author: {
+        _id: "author3",
+        name: "Sam Green",
+        picture: "profile3.jpg",
+        clerkId: "clerk3",
+      },
+      upvotes: ["user6", "user7", "user8"],
+      views: 1200,
+      answers: [
+        {
+          text: "You can use caching techniques to improve performance.",
+          author: "Emily White",
+        },
+        {
+          text: "Optimizing database queries can also enhance performance.",
+          author: "Michael Black",
+        },
+      ],
+      createdAt: new Date(2024, 1, 30),
+    },
+  ];
 
   return (
     <>
@@ -74,8 +150,8 @@ export default async function Home({ searchParams }: SearchParamsProps) {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {/* {result?.questions?.length > 0 ? (
-          result?.questions?.map((question) => (
+        {questions?.length > 0 ? (
+          questions?.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
@@ -95,13 +171,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
             link="/ask-question"
             linkTitle="Ask a Question"
           />
-        )} */}
-        <NoResult
-          title="There’s no question to show"
-          description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
-          link="/ask-question"
-          linkTitle="Ask a Question"
-        />
+        )}
       </div>
       <div className="mt-10">
         {/* <Pagination 
